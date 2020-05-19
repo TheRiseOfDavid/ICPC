fc3263ac952744803e1f9aa07ade0bf1452e17d4
TheRiseOfDavid <david53133@gmail.com>
2020-05-19 22:12:48 +0800

    20200519.2212

--- ICPC 2019-2020 North-Western Russia Regional Contest/Problem_E/main.cpp
+++ ICPC 2019-2020 North-Western Russia Regional Contest/Problem_E/main.cpp
@@ -17,13 +17,13 @@ void BFS_to_large_path(int root ){
         if(!visit[node]){
             BFS_to_large_path(node);
             travel.pop_back();
-            visit[root] = 0 ;
         }
     }
-    //debug to check large path
-    //if (root == 1)
-    //    cout << "1=" << travel.size() << ' ' << Maxn_path << ' ' << city[root] << '\n' ;
 
+    //debug to check large path
+    //cout << root << ' ' << travel.size() << ' ' << Maxn_path << ' ' << city[root] << '\n' ;
+
+    visit[root] = 0 ;
     if(city[root] && travel.size() > Maxn_path){
         Maxn_city = travel[travel.size()/2];
         Maxn_path = travel.size();
@@ -34,16 +34,15 @@ void BFS_to_other_path(int root ,int path){
     visit[root] = 1 ;
     for(int i = 0 ; i < tree[root].size() ; i++){
         int node = tree[root][i] ;
-        if(!visit[node]){
+        if(!visit[node])
             BFS_to_other_path(node , path+1);
-            visit[root] = 0 ;
-        }
     }
-    //debug
-    if(root == 1 )
-        cout << "city=" << root << " path= " << path << '\n' ;
+    visit[root] = 0 ;
 
-    if(city[root] && path != Maxn_path)
+    //debug
+    //cout << "city=" << root << " path= " << path << '\n' ;
+
+    if(city[root] && path != Maxn_path / 2 +1)
         flag = 0 ;
 }
 
@@ -65,7 +64,13 @@ int main(){
         city[a] = 1 ;
     }
     BFS_to_large_path(a);
-    //visit[a] = 0 ;
+
+    // each visit element must be false
+    //for(int i = 0 ; i <= n ; i++){
+    //    cout << visit[i] << ' ';
+    //}
+    //cout << '\n' ;
+
     BFS_to_other_path(Maxn_city , 1 );
     if(flag)
         cout << "YES\n" << Maxn_city ;
@@ -73,5 +78,5 @@ int main(){
         cout << "NO" ;
 
     //debug
-    cout << "Maxn_path= " << Maxn_path << " Maxn_city= " << Maxn_city << '\n' ;
+    //cout << "Maxn_path= " << Maxn_path << " Maxn_city= " << Maxn_city << '\n' ;
 }
